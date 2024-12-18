348942386fa143ba9267b757027b333e2361a80f
dylan h <baolong281@gmail.com>
2024-12-18 10:35:11 -0600

    better description

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -11,8 +11,19 @@ test_that("Check if main function works", {
                                         boot_num = boot_num,
                                         ncores = 1)
 
+  # Check all types match
   expect_equal(boot_num, length(res$dist))
   expect_type(res$p, "double")
   expect_type(res$stat, "double")
   expect_true(is.vector(res$dist))
+})
+
+test_that("create_kernel_plot generates a plot without errors", {
+  X <- seq(0, 1, length.out = 50)
+  Y <- sin(2 * pi * X) + rnorm(50, sd = 0.1)
+
+  plot_obj <- create_kernel_plot(X, Y)
+
+  # Check that the returned object is of class "recordedplot"
+  expect_true(inherits(plot_obj, "recordedplot"))
 })
