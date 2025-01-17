8c57d3130df76e91318c3d4f267e801201c9f02d
dylan h <baolong281@gmail.com>
2025-01-17 11:39:31 -0600

    multiple bandwidth visualization

--- R/main.R
+++ R/main.R
@@ -213,11 +213,16 @@ validate_inputs <- function(X, Y, m=NULL, monotonicity=TRUE) {
 #'
 #' @param X Vector of x values.
 #' @param Y Vector of y values.
-#' @param bandwidth Kernel bandwidth used for the Nadaraya-Watson estimator.
+#' @param bandwidth Kernel bandwidth used for the Nadaraya-Watson estimator. Can
+#'                  be a single numeric value or a vector of bandwidths.
 #'                  Default is calculated as
 #'                  \code{bw.nrd(X) * (length(X) ^ -0.1)}.
-#' @return A ggplot object containing the scatter plot with the kernel
-#'         regression curve.
+#' @param nrows Number of rows in the facet grid if multiple bandwidths are provided.
+#'              Does not do anything if only a single bandwidth value is provided.
+#'              Default is \code{4}.
+#' @return A ggplot object containing the scatter plot(s) with the kernel
+#'         regression curve(s). If a vector of bandwidths is supplied, the plots
+#'         are put into a grid using faceting.
 #' @references
 #'   Nadaraya, E. A. (1964). On estimating regression. \emph{Theory of
 #'   Probability and Its Applications}, \strong{9}(1), 141–142.
@@ -235,25 +240,50 @@ validate_inputs <- function(X, Y, m=NULL, monotonicity=TRUE) {
 #'
 #' @export
 create_kernel_plot <-
-  function(X, Y, bandwidth = bw.nrd(X) * (length(X) ^ -0.1)) {
+  function(X, Y, bandwidth = bw.nrd(X) * (length(X) ^ -0.1), nrows=4) {
     validate_inputs(X, Y, monotonicity=FALSE)
+
+    if(!is.numeric(bandwidth)) {
+      stop("'bandwidth' must be numeric value or vector")
+    }
+
+    # Check if nrows is an integer greater than zero
+    if(nrows%%1!=0 | nrows<=0) {
+      stop("'nrows' must be an integer greater than zero")
+    }
+
+    bandwidth_list <- as.list(bandwidth)
 
     # Set up the range for the x-axis
     x_range <- seq(min(X), max(X), length.out = 500)
-    y_values <- watson_est(X, Y, x_range, bandwidth = bandwidth)
 
-    # Create plot
-    plot <- ggplot(data.frame(X, Y), aes(x = X, y = Y)) +
-      geom_point() +
+    # Map the bandwidths into dataframes with their respective points
+    # then concat with rbind
+    plot_data <- do.call(rbind, Map(function(bw) {
+      y_vals <- watson_est(X, Y, x_range, bandwidth = bw)
+      data.frame(x = x_range,
+                 y = y_vals,
+                 bandwidth = sprintf("bandwidth = %.3f", bw))
+    }, bandwidth_list))
+
+    points_df <- data.frame(X = X, Y = Y)
+
+    # Atleast 1 column
+    n_cols <- max(length(bandwidth_list) %/% nrows, 1)
+
+    # Create facet wrap / grid plot
+    plot <- ggplot() +
+      geom_point(data = points_df, aes(x = .data$X, y = .data$Y)) +
       geom_line(
-        data = data.frame(x = x_range, y = y_values),
+        data = plot_data,
         aes(x = .data$x, y = .data$y),
         color = "green",
         linewidth = 1.5
       ) +
-      ggtitle(paste("Nadaraya Watson", "bandwidth =", round(bandwidth, 4))) +
-      xlab("X") +
-      ylab("Y")
+      facet_wrap( ~ bandwidth, ncol = n_cols) +
+      labs(title = "Nadaraya Watson Kernel Regression",
+           x = "X",
+           y = "Y")
 
     return(plot)
   }
